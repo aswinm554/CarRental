3d036ff20d443eaa712c78c11d09868ab24d4eae
ASWIN <aswinmepps@gmail.com>
2025-10-23 20:13:11 +0530

    first commit

---
+++ src/pages/Home.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import Hero from '../components/Hero'
+import FaeturedSection from '../components/FaeturedSection'
+
+const Home = () => {
+    return (
+        <>
+            <Hero />
+            <FaeturedSection />
+        </>
+    )
+}
+
+export default Home
